62de7e26c4611748a773b3d43f44d7fdfd601a0a
Al0ha0e <1919270783@qq.com>
2021-12-01 12:20:11 +0800

    gen prefiltered

--- call_tools.py
+++ call_tools.py
@@ -4,3 +4,4 @@ import os
 # os.system("gen_six.exe irr ./assets/textures/hdr/Arches_E_PineTree_3k.hdr ./assets/textures/skybox/ 256")
 os.system("gen_one.exe skybox ./assets/textures/hdr/Arches_E_PineTree_3k.hdr ./assets/textures/skybox/")
 os.system("gen_one.exe irr ./assets/textures/hdr/Arches_E_PineTree_3k.hdr ./assets/textures/skybox/")
+os.system("gen_one.exe pref ./assets/textures/hdr/Arches_E_PineTree_3k.hdr ./assets/textures/skybox/")
--- tools/irr_gen/gen_one.cpp
+++ tools/irr_gen/gen_one.cpp
@@ -86,7 +86,11 @@ int main(int argc, char *argv[])
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
+        if (mode == "pref")
+        {
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
         stbi_image_free(data);
     }
     else
@@ -101,20 +105,31 @@ int main(int argc, char *argv[])
     // -----------------------------
 
     std::shared_ptr<common::ShaderProgram> shader;
-
+    unsigned int max_miplevel;
     if (mode == "skybox")
     {
         shader = std::make_shared<common::ShaderProgram>(
             common::Shader("./tools/irr_gen/gen1.vs", common::VERTEX_SHADER),
             common::Shader("./tools/irr_gen/gen1.fs", common::FRAGMENT_SHADER));
+        max_miplevel = 1;
     }
     else if (mode == "irr")
     {
         shader = std::make_shared<common::ShaderProgram>(
             common::Shader("./tools/irr_gen/gen1.vs", common::VERTEX_SHADER),
             common::Shader("./tools/irr_gen/gen_irr1.fs", common::FRAGMENT_SHADER));
+        max_miplevel = 1;
         width >>= 3;
         height >>= 3;
+    }
+    else if (mode == "pref")
+    {
+        shader = std::make_shared<common::ShaderProgram>(
+            common::Shader("./tools/irr_gen/gen1.vs", common::VERTEX_SHADER),
+            common::Shader("./tools/irr_gen/gen_pref1.fs", common::FRAGMENT_SHADER));
+        max_miplevel = 5;
+        width >>= 4;
+        height >>= 4;
     }
 
     glUseProgram(shader->shader);
@@ -148,31 +163,45 @@ int main(int argc, char *argv[])
     glRenderbufferStorage(GL_RENDERBUFFER, GL_RGB, width, height);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, captureRBO);
 
-    glUseProgram(shader->shader);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, hdrTexture);
-    glBindVertexArray(vao);
+    for (int i = 0; i < max_miplevel; i++)
+    {
+        glUseProgram(shader->shader);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, hdrTexture);
+        if (mode == "pref")
+        {
+            float roughness = 1.0f * i / (max_miplevel - 1);
+            glUniform1f(glGetUniformLocation(shader->shader, "roughness"), roughness);
+        }
 
-    glViewport(0, 0, width, height);
+        glBindVertexArray(vao);
 
-    glClear(GL_COLOR_BUFFER_BIT);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+        glViewport(0, 0, width, height);
 
-    glBindBuffer(GL_PIXEL_PACK_BUFFER, pbo);
-    glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-    void *scrdata = glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_WRITE);
-    stbi_write_png((outpath + mode + ".png").c_str(), width, height, 3, scrdata, 0);
-    glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, pbo);
+        glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+        void *scrdata = glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_WRITE);
+        stbi_write_png((outpath + mode + std::string("_mip") + std::to_string(i) + ".png").c_str(), width, height, 3, scrdata, 0);
+        glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+        width >>= 1;
+        height >>= 1;
+    }
     std::ofstream outjson(outpath + mode + ".json");
     outjson << "{\n";
     outjson << "\"wraps\":" << GL_CLAMP_TO_EDGE << ",\n";
     outjson << "\"wrapt\":" << GL_CLAMP_TO_EDGE << ",\n";
-    outjson << "\"minfilter\":" << GL_LINEAR << ",\n";
+    outjson << "\"minfilter\":" << (mode == "pref" ? GL_LINEAR_MIPMAP_LINEAR : GL_LINEAR) << ",\n";
     outjson << "\"magfilter\":" << GL_LINEAR << ",\n";
-    outjson << "\"miplevel\":1,\n";
+    outjson << "\"miplevel\":" << max_miplevel << ",\n";
     outjson << "\"automip\":false,\n";
-    outjson << "\"paths\":[\"" << outpath + mode + ".png"
-            << "\"]\n";
+    outjson << "\"paths\":[\n";
+    for (int i = 0; i < max_miplevel; i++)
+        outjson << "\"" << outpath + mode + std::string("_mip") + std::to_string(i) + ".png"
+                << "\"" << (i == max_miplevel - 1 ? "\n" : ",\n");
+    outjson << "]\n";
     outjson << "}\n";
     outjson.close();
 
